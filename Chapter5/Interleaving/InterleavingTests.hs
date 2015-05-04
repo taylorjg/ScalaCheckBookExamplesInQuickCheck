7bc153d9b43e965a1cf3a678bf291aef50bbca19
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-05-04 10:12:50 +0100

    Converted tabs to spaces

--- Chapter5/Interleaving/InterleavingTests.hs
+++ Chapter5/Interleaving/InterleavingTests.hs
@@ -3,14 +3,14 @@ import Interleaving (interleave)
 
 interleaveProperty :: [Int] -> [Int] -> Property
 interleaveProperty xs ys =
-	conjoin [p1, p2, p3]
-	where
-		res = interleave xs ys
-		is = [0 .. min (length xs) (length ys) - 1]
-		p1 = label "length" $ length xs + length ys == length res
-		p2 = label "zip xs" $ xs == map (\i -> res !! (2*i)) is ++ drop (2*length ys) res
-		p3 = label "zip ys" $ ys == map (\i -> res !! (2*i+1)) is ++ drop (2*length xs) res
+    conjoin [p1, p2, p3]
+    where
+        res = interleave xs ys
+        is = [0 .. min (length xs) (length ys) - 1]
+        p1 = label "length" $ length xs + length ys == length res
+        p2 = label "zip xs" $ xs == map (\i -> res !! (2*i)) is ++ drop (2*length ys) res
+        p3 = label "zip ys" $ ys == map (\i -> res !! (2*i+1)) is ++ drop (2*length xs) res
 
 main :: IO ()
 main = do
-	quickCheck interleaveProperty
+    quickCheck interleaveProperty
