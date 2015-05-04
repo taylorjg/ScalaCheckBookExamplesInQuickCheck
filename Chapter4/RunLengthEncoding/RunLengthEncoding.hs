0725c6b4a93924c47ff6c0473880b75bf8b1c6de
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-05-04 21:34:33 +0100

    Converted tabs to spaces

--- Chapter4/RunLengthEncoding/RunLengthEncoding.hs
+++ Chapter4/RunLengthEncoding/RunLengthEncoding.hs
@@ -2,16 +2,16 @@ module RunLengthEncoding (runLengthEnc, runLengthDec) where
 
 runLengthEnc :: Eq a => [a] -> [(Int, a)]
 runLengthEnc xs =
-	loop xs Nothing
-	where
-		loop :: Eq a => [a] -> Maybe (Int, a) -> [(Int, a)]
-		loop xs currTuple =
-			case (xs, currTuple) of
-				([], Nothing) -> []
-				([], Just(t)) -> [t]
-				(x:rest, Nothing) -> loop rest (Just (1, x))
-				(x1:rest, Just((n, x2))) | x1 == x2 -> loop rest (Just (n + 1, x2))
-				(x:rest, Just(t)) -> t:loop rest (Just (1, x))
+    loop xs Nothing
+    where
+        loop :: Eq a => [a] -> Maybe (Int, a) -> [(Int, a)]
+        loop xs currTuple =
+            case (xs, currTuple) of
+                ([], Nothing) -> []
+                ([], Just(t)) -> [t]
+                (x:rest, Nothing) -> loop rest (Just (1, x))
+                (x1:rest, Just((n, x2))) | x1 == x2 -> loop rest (Just (n + 1, x2))
+                (x:rest, Just(t)) -> t:loop rest (Just (1, x))
 
 runLengthDec :: [(Int, a)] -> [a]
 runLengthDec = (>>= uncurry replicate)
