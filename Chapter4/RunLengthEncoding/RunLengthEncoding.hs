1035f834e308435df779eb402641fe70a6715559
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-05-06 11:42:42 +0100

    Simplified runLengthEnc by use of 'break'

--- Chapter4/RunLengthEncoding/RunLengthEncoding.hs
+++ Chapter4/RunLengthEncoding/RunLengthEncoding.hs
@@ -1,17 +1,11 @@
 module RunLengthEncoding (runLengthEnc, runLengthDec) where
 
 runLengthEnc :: Eq a => [a] -> [(Int, a)]
-runLengthEnc xs =
-    loop xs Nothing
+runLengthEnc [] = []
+runLengthEnc xs@(hd:_) =
+    (length matching, hd):runLengthEnc nonMatching
     where
-        loop :: Eq a => [a] -> Maybe (Int, a) -> [(Int, a)]
-        loop xs currTuple =
-            case (xs, currTuple) of
-                ([], Nothing) -> []
-                ([], Just(t)) -> [t]
-                (x:rest, Nothing) -> loop rest (Just (1, x))
-                (x1:rest, Just((n, x2))) | x1 == x2 -> loop rest (Just (n + 1, x2))
-                (x:rest, Just(t)) -> t:loop rest (Just (1, x))
+        (matching, nonMatching) = break (/=hd) xs
 
 runLengthDec :: [(Int, a)] -> [a]
 runLengthDec = (>>= uncurry replicate)
