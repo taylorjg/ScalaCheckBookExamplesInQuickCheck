4b398c35ebce06b4294218a00054d576bfad9fc8
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-07-05 19:12:52 +0100

    A bit more work on the custom test case simplification example

--- src/Chapter6/CustomTestCaseSimplification/Test.hs
+++ src/Chapter6/CustomTestCaseSimplification/Test.hs
@@ -4,16 +4,25 @@ data Expression
     = Const Int
     | Add Expression Expression
     | Mul Expression Expression
+    deriving Eq
 
 instance Show Expression where
     show (Const n) = show n
     show (Add e1 e2) = "(" ++ show e1 ++ " + " ++ show e2 ++ ")"
     show (Mul e1 e2) = "(" ++ show e1 ++ " * " ++ show e2 ++ ")"
 
--- eval :: Expression -> Int
--- eval (Const n) = n
--- eval (Add e1 e2) = eval e1 + eval e2
--- eval (Mul e1 e2) = eval e1 * eval e2
+eval :: Expression -> Int
+eval (Const n) = n
+eval (Add e1 e2) = eval e1 + eval e2
+eval (Mul e1 e2) = eval e1 * eval e2
+
+rewrite :: Expression -> Expression
+rewrite e = case e of
+    (Add e1 e2) | e1 == e2 -> Mul (Const 2) e1
+    (Mul (Const 0) _) -> Const 0
+    (Add (Const 1) e2) -> e2
+    -- (Add (Const 0) e2) -> e2
+    _ -> e
 
 genExpr :: Gen Expression
 genExpr =
@@ -39,9 +48,9 @@ genMul = do
     e2 <- genExpr
     return $ Mul e1 e2
 
--- propRewrite :: Property
--- propRewrite = undefined
+propRewrite :: Property
+propRewrite = forAll genExpr $ \expr ->
+    eval (rewrite expr) == eval expr
 
 main :: IO ()
--- main = return ()
-main = sample genExpr
+main = quickCheckWith (stdArgs { maxSuccess = 10000 }) propRewrite
